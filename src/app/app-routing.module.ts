1e6c83a915973afaa10ca338dbd37986eb7d966a
PiotrekK2001 <97753158+PiotrekK2001@users.noreply.github.com>
2023-03-06 18:28:46 +0100

    routing and auth guard

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,28 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AuthGuard } from './auth.guard';
+import { FavoriteComponent } from './layout/components/favorite/favorite.component';
+import { HomeComponent } from './layout/components/home/home.component';
+import { SearchComponent } from './layout/components/search/search.component';
+import { TrackComponent } from './layout/components/track/track.component';
+import { RegainAccessComponent } from './login/components/regain-access/regain-access.component';
+import { RegisterComponent } from './login/components/register/register.component';
+import { LoginComponent } from './login/login.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: 'login', pathMatch: 'full'},
+  
+  { path: 'login', component: LoginComponent},
+  { path: 'register', component: RegisterComponent },
+  { path: 'regain-access', component: RegainAccessComponent },
+
+  { path: 'home', component: HomeComponent, canActivate: [AuthGuard] },
+  { path: 'search', component: SearchComponent, canActivate: [AuthGuard] },
+  { path: 'favorite', component: FavoriteComponent, canActivate: [AuthGuard] }, 
+
+  { path: 'track', component: TrackComponent, canActivate: [AuthGuard] }, 
+];
+
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
