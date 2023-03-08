d79ad4bf1a720220471ce6632e9c56344289d063
apilak <adam.pilak@emkaem.pl>
2023-03-08 13:19:23 +0100

    ukonczony sidebar + logika nawigacji

--- src/app/layout/components/navigation/navigation.component.ts
+++ src/app/layout/components/navigation/navigation.component.ts
@@ -1,23 +1,38 @@
 import { Component, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
+import {
+  ActivatedRoute,
+  ChildActivationStart,
+  NavigationStart,
+  Router,
+} from '@angular/router';
+import { filter, Subscribable, Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-navigation',
   templateUrl: './navigation.component.html',
-  styleUrls: ['./navigation.component.scss']
+  styleUrls: ['./navigation.component.scss'],
 })
 export class NavigationComponent implements OnInit {
-
   isSidebarExpanded: boolean = false;
+  actualPage: string = '';
+  routeParamsSubscription?: Subscription;
 
-  constructor(private router: Router) { }
+  constructor(public router: Router, private route: ActivatedRoute) {}
 
   ngOnInit(): void {
+    this.actualPage = this.router.routerState.snapshot.url;
+
+    this.router.events
+    .pipe(filter(event => event instanceof NavigationStart))
+    .subscribe(event => {
+      this.actualPage = (event as NavigationStart).url;
+    });
   }
-  
+
   logOut(): void {
     localStorage.removeItem('user');
     this.router.navigate(['/login']);
   }
+
 
 }
--- src/app/layout/layout.component.ts
+++ src/app/layout/layout.component.ts
@@ -1,15 +1,18 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, NavigationEnd, NavigationStart, Router } from '@angular/router';
+import { filter, Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-layout',
   templateUrl: './layout.component.html',
-  styleUrls: ['./layout.component.scss']
+  styleUrls: ['./layout.component.scss'],
 })
 export class LayoutComponent implements OnInit {
+  constructor(private route: ActivatedRoute, private router: Router) {}
 
-  constructor() { }
+  routeParamsSubscription?: Subscription;
 
   ngOnInit(): void {
-  }
 
+  }
 }
